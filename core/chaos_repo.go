f13f3fb0c203ae7049ae1c897161c2087f4cbf65
zhangdonghao <zhangdonghao@matrixorigin.cn>
2023-12-08 18:44:01 +0800

    remove retry

--- core/chaos_repo.go
+++ core/chaos_repo.go
@@ -1,17 +1,16 @@
 package core
 
 import (
-	"github.com/sirupsen/logrus"
 	"os/exec"
+
+	"github.com/sirupsen/logrus"
 )
 
 func CloneChaosRepo() {
 	cmd := exec.Command("git", "clone", "-b", ChaosGitBranch, ChaosGitAddress)
 	out, err := cmd.CombinedOutput()
 	if err != nil {
-		logrus.Info(string(out))
-		// retry
-		CloneChaosRepo()
+		logrus.Fatal(string(out))
 	}
 	logrus.Info(string(out))
 }
